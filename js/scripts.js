10172dec9ba57fe37c9b100e0df1e509a64f3a52
Akigawa <sonoskeakigawa@gmail.com>
2019-04-08 16:04:37 -0700

    Temba! His eyes uncovered.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,75 @@
+var newacc;
+var accountBook;
 $(document).ready(function() {
-    testacc = new Account(Brian Pritt, 0)
-   // Stuff to do as soon as the DOM is ready
+  accountBook = new AccountBook();
+  var planbook = [];
+  var printer;
+  accountBook.newid = 0;
+  planbook[0] = new Plan(35, 1, "personal");
+  planbook[1] = new Plan(20, 2, "business");
+  planbook[2] = new Plan(10, 5, "corporate");
+  $("#plans").append("<fieldset>");
+  planbook.forEach(function(y){
+  printer = "<input type='radio' name='plan' value='";
+  printer += y.name + "'" + ">";
+  printer += "</input>"+ y.name + "<br>";
+  $("#plans").append(printer);
+  console.log(y.name);
+});
+$("#plans").append("</fieldset>");
+  testacc = new Account("Brian Pritt", 0, [], planbook[0]);
+  accountBook[accountBook.newid] = testacc;
    $("#registerform").submit(function(){
      event.preventDefault();
+     newacc = new Account($("#newname").val(),$("#initdeposit").val(),[$("#initdeposit").val()],planbook[0]);
+     accountBook.newid++;
+     console.log(accountBook.book);
+     accountBook.book[accountBook.newid] = newacc;
+     console.log(accountBook.book[accountBook.newid].name);
+     printer = ""
+     printer += "<option value='" + accountBook.newid;
+     printer += "'>"+ accountBook.book[accountBook.newid].name +  "</option>";
+     $("#accts").append(printer);
    });
    $("#DWform").submit(function(){
      event.preventDefault();
-     $("#receipt").html();
-     $("#receipt").append("asdfgasdgasd");
+     accountBook.currid= $("#accts").val();
 
+     $("#receiptheader").empty();
+     $("#receipt").empty();
+     console.log(accountBook.book[accountBook.currid]);
+     ChangeMoney(accountBook.book[accountBook.currid], $("#withdraw").val(), $("#deposit").val());
+     $("#receipt").append("<br> <br>Deposit:<br>" + $("#deposit").val() + "<br> Withdrawal: " + $("#withdraw").val() +"<br>" + accountBook.book[accountBook.currid].balance );
+     $("#receiptheader").append( "<br>" +accountBook.book[accountBook.currid].name );
    });
 });
-function AccountBook(book, id){
+function AccountBook(book, newid,currid){
 this.book = [];
-this.curid = id;
-
+this.newid = newid;
+this.plans = [];
+this.curracc;
+this.currid = currid;
 }
-function Account(name, balance, history, rate)
-{
+function Plan(fee, interest,name){
+this.interest = interest;
+this.fee = fee;
+this.name = name;
+}
+
+function Account(name, balance, history, plan){
 this.name = name;
 this.balance = balance;
 this.history = [];
-this.rate = rate;
+this.plan = plan;
 }
-function AddMoney(acc,withdrawal,deposit)
-{
+function ChangeMoney(acc,withdrawal,deposit){
 
-acc.balance += withdrawal + deposit;
+acc.balance = parseFloat(acc.balance) - parseFloat(withdrawal) + parseFloat(deposit);
+if(acc.balance < 0){
+  acc.balance -= acc.plan.fee;
 
+}
+acc.balance = parseFloat(acc.balance).toFixed(2);
 }
 
 function YearPass(rate, interest, balance){
